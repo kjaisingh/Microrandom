9bc9f174de75e3a0b22751b761d4f6ed57932c11
Karan Jaisingh <kj.jaisingh@gmail.com>
2020-07-06 21:22:10 +0800

    Implemented first-pass scoring algorithm

--- microrandom.js
+++ microrandom.js
@@ -1,7 +1,6 @@
-const geneticAlgorithm = require('./genetic.js');
 
+// function to generate initial groups
 function generateGroups(members, groupCount) {
-
   var initialGroups = [];
   for (var i = 0; i < 10; i++) {
     initialGroups.push([]);
@@ -36,20 +35,65 @@ function generateGroups(members, groupCount) {
   return groupings;
 }
 
+// function to shuffle items in array
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
-
   while (0 !== currentIndex) {
-
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
-
     temporaryValue = array[currentIndex];
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
   }
-
   return array;
+}
+
+// function to manage genetic algorithm
+function geneticAlgorithm(initialGroups) {
+  const scores = scoringSeeds(initialGroups);
+
+  return initialGroups;
+}
+
+// higher score = greater variation
+// function to score seed groups
+function scoringSeeds(groups) {
+  var scores = []
+  scores = Array(groups.length).fill(0);
+  var index = 0;
+
+  groups.forEach(function(group) {
+    group.forEach(function(subgroup) {
+
+      var i;
+      var j;
+      var subscores = [0.0, 0.0, 0.0, 0.0];
+
+
+      for(i = 0; i < subgroup.length; i++) {
+        for(j = 0; j < subgroup.length; j++) {
+          if(i !== j) {
+            var ageScore = Math.abs(subgroup[i].age - subgroup[j].age);
+            subscores[0] +=  ageScore / 4;
+            subscores[1] += (subgroup[i].gender === subgroup[j].gender) ? 0 : 1;
+            subscores[2] += (subgroup[i].ethnicity === subgroup[j].ethnicity) ? 0 : 1;
+            subscores[3] += (subgroup[i].religion === subgroup[j].religion) ? 0 : 1;
+          }
+        }
+      }
+
+      subscore = subscores.reduce((a, b) => a + b, 0);
+      scores[index] += subscore / (2 * subgroup.length);
+      
+    });
+
+    index += 1;
+
+  });
+
+  console.log(scores);
+
+  return scores;
 }
 
 module.exports = generateGroups;
