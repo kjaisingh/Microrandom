8aca4bd5a83b9063961cbdc5ec5a42bd3013a488
Karan Jaisingh <kj.jaisingh@gmail.com>
2020-07-08 13:08:00 +0800

    Heroku creation

--- app.js
+++ app.js
@@ -488,7 +488,11 @@ app.post("/:userId/:groupId/joined-group", function(req, res) {
 // -------------------------
 // SETUP
 // -------------------------
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000.");
+app.listen(port, function() {
+  console.log("Server has started successfully.");
 });
